92b02af094b799da32a52daa6f9d3aa98f9964c3
TanBryant <nhattan0932507137@gmail.com>
2023-06-22 09:03:48 +0700

    done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,62 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sidebar.scss";
-import Logo from '../../assets/logo.svg'
+import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
+  const [toggle, showMenu] = useState(false);
   return (
-    <aside className="aside">
-      <a href="#home" className="nav__logo">
-        <img src={Logo} alt="" />
-      </a>
+    <>
+      <aside className={toggle ? "aside show-menu" : "aside"}>
+        <a href="#home" className="nav__logo">
+          <img src={Logo} alt="" />
+        </a>
 
-      <nav className="nav">
-        <div className="nav_menu">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="icon-home"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#about" className="nav__link">
-                <i className="icon-user-following"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#service" className="nav__link">
-                <i className="icon-briefcase"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#resume" className="nav__link">
-                <i className="icon-graduation"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
-                <i className="icon-layers"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#blog" className="nav__link">
-                <i className="icon-note"></i>
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="contact" className="nav__link">
-                <i className="icon-bubble"></i>
-              </a>
-            </li>
-          </ul>
+        <nav className="nav">
+          <div className="nav_menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="#home" className="nav__link">
+                  <i className="icon-home"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#about" className="nav__link">
+                  <i className="icon-user-following"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#service" className="nav__link">
+                  <i className="icon-briefcase"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#resume" className="nav__link">
+                  <i className="icon-graduation"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#portfolio" className="nav__link">
+                  <i className="icon-layers"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#blog" className="nav__link">
+                  <i className="icon-note"></i>
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="contact" className="nav__link">
+                  <i className="icon-bubble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="nav__footer">
+          <span className="copyright">&copy; 2022 - 2023.</span>
         </div>
-      </nav>
-
-      <div className="nav__footer">
-        <span className="copyright">
-          &copy; 2022 - 2023.
-        </span>
+      </aside>
+      <div className={toggle ? "nav__toggle nav__toggle-open" : 
+        "nav__toggle"} onClick={() => showMenu(!toggle)}>
+        <i className="icon-menu"></i>
       </div>
-    </aside>
+    </>
   );
 };
 
